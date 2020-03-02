03cbf7f6f9840cd7c8e1e54bf6c0ed05ef2b1fab
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-03-02 04:03:08 +0000

    Automatically backed up by Learn

--- db/migrate/20200302040235_preferences.rb
+++ db/migrate/20200302040235_preferences.rb
@@ -1,6 +1,6 @@
 class Preferences < ActiveRecord::Migration[5.0]
   def change
-    create_table :preferences do |t|
+      create_table :preferences do |t|
         t.string :artist_sort_order
         t.string :song_sort_order
         t.boolean :allow_create_artists
